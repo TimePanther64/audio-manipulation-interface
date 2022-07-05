7ce0aa45b39c9c9c4d17f06cf2faf14283ea694b
Jayatu Barua <98374934+TimePanther64@users.noreply.github.com>
2022-07-05 19:43:19 +0530

    Update stereo_to_ambisonic_converter.py

--- stereo_to_ambisonic_converter.py
+++ stereo_to_ambisonic_converter.py
@@ -10,6 +10,7 @@ from tkinter import Toplevel, filedialog as fd, ttk
 from pathlib import Path
 from pydub import AudioSegment
 from pydub.playback import play
+from pydub.silence import split_on_silence
 from math import *
 from glob import glob
 from scipy.signal import butter, lfilter
@@ -18,96 +19,132 @@ class guiApp(object):
   def __init__(self):
     self.root = tk.Tk()
     # self.root.tk.call('wm', 'iconphoto', self.root._w, tk.PhotoImage(file='/Users/barua/OneDrive/Documents/Internship/Tkinter/audio/headphones.png'))
-    self.root.geometry("600x800")
-    self.root.title("Audio Converter")
+    self.root.geometry("600x600")
+    self.root.title("Audio Processing Software")
     self.style = ttk.Style().theme_use("xpnative")
 
-    self.theme_frame_single = ttk.LabelFrame(self.root, text="Single File Coversion")
-    self.theme_frame_single.place(relx=.125, rely=.03, relwidth=.75, relheight=.3)
+    self.tabControl = ttk.Notebook(self.root)
+    self.tab1 = ttk.Frame(self.tabControl)
+    self.tab2 = ttk.Frame(self.tabControl)
+    self.tab3 = ttk.Frame(self.tabControl)
+    self.tabControl.add(self.tab1, text='Ambisonic Conversion')
+    self.tabControl.add(self.tab2, text='Audio Enhancement')
+    self.tabControl.add(self.tab3, text='Audio Trimmer')
+    self.tabControl.place(relx=0.025, rely=0.025, relheight=0.95, relwidth=0.95)
 
-    self.theme_frame_batch = ttk.LabelFrame(self.root, text="Batch File Conversion")
-    self.theme_frame_batch.place(relx=.125, rely=.35, relwidth=.75, relheight=.125)
+    self.theme_frame_single = ttk.LabelFrame(self.tab1, text="Single File Coversion")
+    self.theme_frame_single.place(relx=.125, rely=.03, relwidth=.75, relheight=.5)
 
-    self.theme_frame_modify = ttk.LabelFrame(self.root, text="Modify Audio File")
-    self.theme_frame_modify.place(relx=.125, rely=.5, relwidth=.75, relheight=.425)
+    self.theme_frame_batch = ttk.LabelFrame(self.tab1, text="Batch File Conversion")
+    self.theme_frame_batch.place(relx=.125, rely=.55, relwidth=.75, relheight=.25)
 
-    self.file_label = ttk.Label(self.root, text="Select a file to convert: ")
-    self.file_label.place(relx=.2, rely=.1)
+    self.file_label = ttk.Label(self.tab1, text="Select a file to convert: ")
+    self.file_label.place(relx=.2, rely=.125)
 
-    self.select_button = ttk.Button(self.root, text="Select", command=lambda: self.select_file(1))
-    self.select_button.place(relx=.55, rely=.1)
+    self.select_button = ttk.Button(self.tab1, text="Select", command=lambda: self.select_file(1))
+    self.select_button.place(relx=.55, rely=.125)
 
-    self.select_label = ttk.Label(self.root, text="You have selected: ")
-    self.select_label.place(relx=.2, rely=.18)
+    self.select_label = ttk.Label(self.tab1, text="You have selected: ")
+    self.select_label.place(relx=.2, rely=.255)
 
-    self.show_label = ttk.Label(self.root, text="")
-    self.show_label.place(relx=.5525, rely=.18)
+    self.show_label = ttk.Label(self.tab1, text="")
+    self.show_label.place(relx=.5525, rely=.255)
 
-    self.convert_button = ttk.Button(self.root, text="Convert", command=self.convert_file)
-    self.convert_button.place(relx=.3, rely=.25)
+    self.convert_button = ttk.Button(self.tab1, text="Convert", command=self.convert_file)
+    self.convert_button.place(relx=.3, rely=.4)
 
-    self.play_button = ttk.Button(self.root, text="Play", command=lambda: self.play_file(0))
-    self.play_button.place(relx=.55, rely=.25)
+    self.play_button = ttk.Button(self.tab1, text="Play", command=lambda: self.play_file(0))
+    self.play_button.place(relx=.55, rely=.4)
 
-    self.folder_label = ttk.Label(self.root, text="Select the folder(s) you wish to \nconvert: ")
-    self.folder_label.place(relx=.2, rely=.4)
+    self.folder_label = ttk.Label(self.tab1, text="Select the folder(s) you wish to \nconvert: ")
+    self.folder_label.place(relx=.2, rely=.65)
 
-    self.batch_convert_button = ttk.Button(self.root, text="Batch Convert", command=self.start_batch_convert)
-    self.batch_convert_button.place(relx=.575, rely=.4, relwidth=.25)
+    self.batch_convert_button = ttk.Button(self.tab1, text="Batch Convert", command=self.start_batch_convert)
+    self.batch_convert_button.place(relx=.575, rely=.65, relwidth=.25)
+
+    self.quitButton1 = ttk.Button(self.tab1, text="Quit", command=self.root.quit)
+    self.quitButton1.place(relx=.738, rely=.85)
+
+    self.theme_frame_modify = ttk.LabelFrame(self.tab2, text="Modify Audio File")
+    self.theme_frame_modify.place(relx=.125, rely=.03, relwidth=.75, relheight=.775)
 
     self.current_value_reverb = tk.IntVar()
     self.current_value_treble = tk.IntVar()
     self.current_value_bass = tk.IntVar()
     self.current_value_volume = tk.IntVar()
 
-    self.slider_label_reverb = ttk.Label(self.root, text='Reverb:')
-    self.slider_label_reverb.place(relx=.2, rely=.65)
-    self.slider_reverb = ttk.Scale(self.root, from_=15, to=30, orient='horizontal', command=self.slider_changed_reverb, variable=self.current_value_reverb, length=150)
-    self.slider_reverb.place(relx=.4, rely=.65)
-    self.value_label_reverb = ttk.Label(self.root, text=self.current_value_reverb.get())
-    self.value_label_reverb.place(relx=.7, rely=.65)
+    self.slider_label_reverb = ttk.Label(self.tab2, text='Reverb:')
+    self.slider_label_reverb.place(relx=.2, rely=.375)
+    self.slider_reverb = ttk.Scale(self.tab2, from_=15, to=30, orient='horizontal', command=self.slider_changed_reverb, variable=self.current_value_reverb, length=150)
+    self.slider_reverb.place(relx=.4, rely=.375)
+    self.value_label_reverb = ttk.Label(self.tab2, text=self.current_value_reverb.get())
+    self.value_label_reverb.place(relx=.7, rely=.375)
 
-    self.slider_label_treble = ttk.Label(self.root, text='Treble:')
-    self.slider_label_treble.place(relx=.2, rely=.7)
-    self.slider_treble = ttk.Scale(self.root, from_=2, to=12, orient='horizontal', command=self.slider_changed_treble, variable=self.current_value_treble, length=150)
-    self.slider_treble.place(relx=.4, rely=.7)
-    self.value_label_treble = ttk.Label(self.root, text=self.current_value_treble.get())
-    self.value_label_treble.place(relx=.7, rely=.7)
+    self.slider_label_treble = ttk.Label(self.tab2, text='Treble:')
+    self.slider_label_treble.place(relx=.2, rely=.45)
+    self.slider_treble = ttk.Scale(self.tab2, from_=2, to=12, orient='horizontal', command=self.slider_changed_treble, variable=self.current_value_treble, length=150)
+    self.slider_treble.place(relx=.4, rely=.45)
+    self.value_label_treble = ttk.Label(self.tab2, text=self.current_value_treble.get())
+    self.value_label_treble.place(relx=.7, rely=.45)
 
-    self.slider_label_bass = ttk.Label(self.root, text='Bass:')
-    self.slider_label_bass.place(relx=.2, rely=.75)
-    self.slider_bass = ttk.Scale(self.root, from_=2, to=12, orient='horizontal', command=self.slider_changed_bass, variable=self.current_value_bass, length=150)
-    self.slider_bass.place(relx=.4, rely=.75)
-    self.value_label_bass = ttk.Label(self.root, text=self.current_value_bass.get())
-    self.value_label_bass.place(relx=.7, rely=.75)
+    self.slider_label_bass = ttk.Label(self.tab2, text='Bass:')
+    self.slider_label_bass.place(relx=.2, rely=.525)
+    self.slider_bass = ttk.Scale(self.tab2, from_=2, to=12, orient='horizontal', command=self.slider_changed_bass, variable=self.current_value_bass, length=150)
+    self.slider_bass.place(relx=.4, rely=.525)
+    self.value_label_bass = ttk.Label(self.tab2, text=self.current_value_bass.get())
+    self.value_label_bass.place(relx=.7, rely=.525)
 
-    self.slider_label_volume = ttk.Label(self.root, text="Volume:")
-    self.slider_label_volume.place(relx=.2, rely=.8)
-    self.slider_volume = ttk.Scale(self.root, from_=-36, to=36, orient='horizontal', command=self.slider_changed_volume, variable=self.current_value_volume, length=150)
-    self.slider_volume.place(relx=.4, rely=.8)
-    self.value_label_volume = ttk.Label(self.root, text=self.current_value_volume.get())
-    self.value_label_volume.place(relx=.7, rely=.8)
+    self.slider_label_volume = ttk.Label(self.tab2, text="Volume:")
+    self.slider_label_volume.place(relx=.2, rely=.6)
+    self.slider_volume = ttk.Scale(self.tab2, from_=-36, to=36, orient='horizontal', command=self.slider_changed_volume, variable=self.current_value_volume, length=150)
+    self.slider_volume.place(relx=.4, rely=.6)
+    self.value_label_volume = ttk.Label(self.tab2, text=self.current_value_volume.get())
+    self.value_label_volume.place(relx=.7, rely=.6)
 
-    self.file_label_modify = ttk.Label(self.root, text="Select a file to modify: ")
-    self.file_label_modify.place(relx=.2, rely=.55)
+    self.file_label_modify = ttk.Label(self.tab2, text="Select a file to modify: ")
+    self.file_label_modify.place(relx=.2, rely=.125)
 
-    self.select_button_modify = ttk.Button(self.root, text="Select", command=lambda: self.select_file(0))
-    self.select_button_modify.place(relx=.55, rely=.55)
+    self.select_button_modify = ttk.Button(self.tab2, text="Select", command=lambda: self.select_file(0))
+    self.select_button_modify.place(relx=.55, rely=.125)
 
-    self.select_label_modify = ttk.Label(self.root, text="You have selected: ")
-    self.select_label_modify.place(relx=.2, rely=.6)
+    self.select_label_modify = ttk.Label(self.tab2, text="You have selected: ")
+    self.select_label_modify.place(relx=.2, rely=.25)
 
-    self.show_label_modify = ttk.Label(self.root, text="")
-    self.show_label_modify.place(relx=.525, rely=.6)
+    self.show_label_modify = ttk.Label(self.tab2, text="")
+    self.show_label_modify.place(relx=.525, rely=.25)
 
-    self.modifyButton = ttk.Button(self.root, text="Modify", command=lambda: self.modify_volume(self.current_value_volume.get()))
-    self.modifyButton.place(relx=.2, rely=.86)
+    self.modifyButton = ttk.Button(self.tab2, text="Modify", command=lambda: self.modify_volume(self.current_value_volume.get()))
+    self.modifyButton.place(relx=.3, rely=.7)
 
-    self.play_button2 = ttk.Button(self.root, text="Play", command=lambda: self.play_file(1))
-    self.play_button2.place(relx=.4, rely=.86)
+    self.play_button2 = ttk.Button(self.tab2, text="Play", command=lambda: self.play_file(1))
+    self.play_button2.place(relx=.55, rely=.7)
 
-    self.quitButton = ttk.Button(self.root, text="Quit", command=self.root.quit)
-    self.quitButton.place(relx=.6, rely=.86)
+    self.quitButton2 = ttk.Button(self.tab2, text="Quit", command=self.root.quit)
+    self.quitButton2.place(relx=.738, rely=.85)
+
+    self.theme_frame_silence = ttk.LabelFrame(self.tab3, text="Silence Detection")
+    self.theme_frame_silence.place(relx=.125, rely=.03, relwidth=.75, relheight=.5)
+
+    self.file_label2 = ttk.Label(self.tab3, text="Select a file containing ads: ")
+    self.file_label2.place(relx=.2, rely=.125)
+
+    self.select_button2 = ttk.Button(self.tab3, text="Select", command=lambda: self.select_file(2))
+    self.select_button2.place(relx=.55, rely=.125)
+
+    self.select_label2 = ttk.Label(self.tab3, text="You have selected: ")
+    self.select_label2.place(relx=.2, rely=.255)
+
+    self.show_label2 = ttk.Label(self.tab3, text="")
+    self.show_label2.place(relx=.5525, rely=.255)
+
+    self.convert_button2 = ttk.Button(self.tab3, text="Remove Ads", command=self.ad_removal)
+    self.convert_button2.place(relx=.3, rely=.4)
+
+    self.play_button2 = ttk.Button(self.tab3, text="Play", command=lambda: self.play_file(0))
+    self.play_button2.place(relx=.55, rely=.4)
+
+    self.quitButton3 = ttk.Button(self.tab3, text="Quit", command=self.root.quit)
+    self.quitButton3.place(relx=.738, rely=.575)
   
   global file_path
   file_path = ''
@@ -140,12 +177,18 @@ class guiApp(object):
       global ext
       name = Path(str(file_path)).stem
       ext = Path(str(file_path)).suffix
-      if value:
+      if value == 0:
         self.show_label.config(text=name+ext)
+        self.show_label2.config(text='')
         self.show_label_modify.config(text='')
+      elif value == 1:
+        self.show_label.config(text='')
+        self.show_label2.config(text='')
+        self.show_label_modify.config(text=name+ext)
       else:
         self.show_label.config(text='')
-        self.show_label_modify.config(text=name+ext)
+        self.show_label2.config(text=name+ext)
+        self.show_label_modify.config(text='')
 
   def convert_file(self):
     interval = 0.175 * 1000
@@ -213,6 +256,21 @@ class guiApp(object):
     self.show_label_modify.config(text=Path(str(des)).stem+Path(str(des)).suffix)
     out_song = open(des, 'wb')
     song.export(out_song, format=ext[1:])
+
+  def ad_removal(self):
+    global file_path
+    song = AudioSegment.from_mp3(file_path)
+
+    section = split_on_silence(song, min_silence_len = 2500, silence_thresh = -22)
+
+    for i, sec in enumerate(section):
+
+        if len(sec) >= 25000:
+          silence = AudioSegment.silent(duration=1500)
+          sec = silence + sec + silence
+          sec.export("modified.mp3".format(i), bitrate = "192k", format = "mp3")
+    
+    self.show_label2.config(text='modified.mp3')
 
 class mProcess(object):
 
